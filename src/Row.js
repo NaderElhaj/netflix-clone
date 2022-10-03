00b6503aa670bd37a2571db43c00a777c47fcbfd
Nader Elhaj <naderelhaj3@gmail.com>
2022-10-03 15:42:06 +0100

    bulding diferrent rows

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import './Row.css'
-function Row() {
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
   return (
-    <div className='row'>
-
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                key={movie.id}
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
